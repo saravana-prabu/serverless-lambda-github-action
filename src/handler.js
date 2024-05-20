f8e3417b1e297660ec87f202e7862ae9670a120c
Saravana Prabu <saravana.prabu@logixal.com>
2024-05-20 17:27:51 +0530

    secod commit

--- src/handler.js
+++ src/handler.js
@@ -1,6 +1,7 @@
 'use strict';
 
 module.exports.generateRandomNumber = event => {
+  console.log("I am here in handler");
   const randomNumber = parseInt(Math.random() * 100);
   console.log("The random generated integer is: ", randomNumber);
   return randomNumber;
